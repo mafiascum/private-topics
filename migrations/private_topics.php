c07777d6246f9f684da1bc31b6d0d7c1d70aafcb
Charlie Ciccia <cciccia@gmail.com>
2018-03-01 15:21:18 -0600

    move permission type to its own migration

--- migrations/private_topics.php
+++ migrations/private_topics.php
@@ -23,7 +23,6 @@ class private_topics extends \phpbb\db\migration\migration
                     'COLUMNS' => array(
                         'topic_id'             => array('UINT:10', 0),
                         'user_id'              => array('UINT:10', 0),
-                        'permission_type'      => array('UINT:4', 1),
                     ),
                     'PRIMARY_KEY' => array('topic_id', 'user_id'),
                 ),
---
+++ migrations/private_topics_permission_type.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace mafiascum\privateTopics\migrations;
+
+class private_topics_permission_type extends \phpbb\db\migration\migration
+{
+
+    public function effectively_installed()
+    {
+        return $this->db_tools->sql_column_exists($this->table_prefix . 'private_topic_users', 'permission_type');
+    }
+    
+    static public function depends_on()
+    {
+        return array('\mafiascum\privateTopics\migrations\private_topics');
+    }
+
+    public function update_schema()
+    {
+        return array(
+            'add_columns' => array(
+                $this->table_prefix . 'private_topic_users' => array(
+                    'permission_type' => array('UINT:4', 1),
+                ),
+            ),
+        );
+    }
+
+    public function revert_schema()
+    {
+         return array(
+            'drop_columns'   => array(
+                $this->table_prefix . 'private_topic_users' => array(
+                    'permission_type',
+                ),
+            ),
+         );
+    }
+}
